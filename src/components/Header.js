d4eabbeb0d7f70e16438a0edef3bf5d5a37a5360
guruprasad199 <guruprasadsali378@gmail.com>
2022-12-15 14:48:26 +0530

    upated code

---
+++ src/components/Header.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { toggleCart } from '../store/slices/cartSlice';
+
+
+const Header = () => {
+
+    const { cartItems } = useSelector((state) => state.cart);
+
+    const dispatch = useDispatch();
+
+
+    const handleOpenCart = (open) => {
+        dispatch(toggleCart(open));
+    };
+
+
+    const cartQuantity = cartItems.length;
+
+
+    return (
+        <>
+            <header id="header">
+                <div className="container">
+                    <div className="navbar">
+                        <h4>Thali building app</h4>
+                        <div className="nav_menu">
+                            <div
+                                title="Cart"
+                                className="cart_icon"
+                                onClick={() => handleOpenCart(true)}
+                            >
+                                <img src="/images/bag-icon.svg" alt="bag-icon" />
+                                Your order
+                                <span className="badge">{cartQuantity}</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </header>
+        </>
+    );
+};
+
+export default Header;
---
+++ src/data/productsData.js
@@ -0,0 +1,53 @@
+
+const productsData = [
+    {
+        id: 1,
+        img: 'images/chapati.png',
+        rating: "★★★★★",
+        title: "Chapati",
+        price: 15,
+        quantity: 1,
+    },
+    {
+        id: 2,
+        img: "/images/curd.png",
+        rating: "★★★★",
+        title: "Curd",
+        price: 40,
+        quantity: 1,
+    },
+    {
+        id: 3,
+        img: "/images/daalTadka.png",
+        rating: "★★★★★",
+        title: "Dal tadka",
+        price: 140,
+        quantity: 1,
+    },
+    {
+        id: 4,
+        img: "/images/gulabJamun.png",
+        rating: "★★★★★",
+        title: "Gulab jamun",
+        price: 120,
+        quantity: 1,
+    },
+    {
+        id: 5,
+        img: "/images/paneer.png",
+        rating: "★★★★",
+        title: "Paneer masala",
+        price: 240,
+        quantity: 1,
+    },
+    {
+        id: 6,
+        img: "/images/pickel.png",
+        rating: "★★★★",
+        title: "Pickel",
+        price: 50,
+        quantity: 1,
+    }
+];
+
+export default productsData;
